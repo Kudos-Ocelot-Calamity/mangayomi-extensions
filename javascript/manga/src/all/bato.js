175893173d24ad77035088c5e8aac6abeb372c4b
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2025-12-17 17:04:45 -0500

    Update bato.js

--- javascript/manga/src/all/bato.js
+++ javascript/manga/src/all/bato.js
@@ -8,7 +8,7 @@ const mangayomiSources = [{
     "isManga": true,
     "isNsfw": true,
     "itemType": 0,
-    "version": "0.0.8",
+    "version": "0.0.9",
     "pkgPath": "manga/src/all/bato.js",
     "notes": ""
 }];
