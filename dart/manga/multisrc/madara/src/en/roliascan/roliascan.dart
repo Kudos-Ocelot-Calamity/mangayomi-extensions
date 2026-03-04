a15e573eba667a1d86489ee0275432d75e456c6f
Kudos-Ocelot-Calamity <ernestobruno617@gmail.com>
2026-03-04 23:06:56 +0200

    Add RoliaScan source configuration

---
+++ dart/manga/multisrc/madara/src/en/roliascan/roliascan.dart
@@ -0,0 +1,14 @@
+import '../../../../../../../model/source.dart';
+
+Source get roliascanSource => _roliascanSource;
+
+Source _roliascanSource = Source(
+  name: "RoliaScan",
+  baseUrl: "https://roliascan.com",
+  lang: "en",
+  typeSource: "madara",
+  iconUrl:
+      "",
+  dateFormat: "MMMM dd, yyyy",
+  dateFormatLocale: "en_us",
+);
